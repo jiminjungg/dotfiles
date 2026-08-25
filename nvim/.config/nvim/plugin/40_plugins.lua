924306739892cf8c95b8337423a0a5c8a0fbea26
Jimin Jung <219709013+jiminjungg@users.noreply.github.com>
2026-08-25 15:33:21 -0700

    feat(nvim): adopt MiniMax's StyLua configuration

--- nvim/.config/nvim/plugin/40_plugins.lua
+++ nvim/.config/nvim/plugin/40_plugins.lua
@@ -130,7 +130,7 @@ later(function()
     },
     -- Map of filetype to formatters
     -- Make sure that necessary CLI tool is available
-    -- formatters_by_ft = { lua = { 'stylua' } },
+    formatters_by_ft = { lua = { 'stylua' } },
   })
 end)
 
